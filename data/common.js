581c9a6a6a8896ac6df1a4d698ce1f65cd97dc8d
vizhigreen <loginvizhi@gmail.com>
2017-05-09 23:09:24 +0530

    Add files via upload

---
+++ data/common.js
@@ -0,0 +1,9 @@
+var news = "<strong>Products for Sale, Current Offers and Announcements</strong>";
+var saleplot = "<strong>Sale plot of Vizhi Green Expressions</strong>";
+var solar = "<strong>Utilizing solar energy to power up personal and environmental devices</strong>";
+var cctv = "<strong>Environment monitoring and recording</strong>";
+var cleanenergy = "<strong>Renewable energy generator for maintaining residual free clean living space</strong>";
+var wireless = "<strong>Short and long range wireless control of appliances and devices</strong>";
+var diykits = "<strong>Plug and play custom hardware solutions for day-to-day life</strong>";
+var greengadgets = "<strong>Promoting gadgets which helps in building a natural green environment</strong>";
+var tools = "<strong>Utilities and applications for supporting and building green solutions</strong>";
---
+++ data/news.js
@@ -0,0 +1,1 @@
+var products = "Product Prices: 10W Solar Charger @ ₹700, WiFi Switch Control @ ₹500";
